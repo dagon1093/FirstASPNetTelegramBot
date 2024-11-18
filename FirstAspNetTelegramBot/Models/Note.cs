7c19c5a72ef923f09fdb09876e081a15eab251a9
dagon1093 <dagon1093@gmail.com>
2024-11-19 00:46:53 +0300

    Добавление модели Note

---
+++ FirstAspNetTelegramBot/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstAspNetTelegramBot.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public Note() { }
+        public Note(int id, string title, string description, string author)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Author = author;
+        }
+
+    }
+}
