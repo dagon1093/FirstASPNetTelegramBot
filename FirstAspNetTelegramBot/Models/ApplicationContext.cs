3a4c67879afb79a19de8163cb184095a3f7563e8
dagon1093 <dagon1093@gmail.com>
2024-11-20 00:05:37 +0300

    Close #2 #3

---
+++ FirstAspNetTelegramBot/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstAspNetTelegramBot.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=NotesDb;Username=postgres;Password=Rp_9i7g7");
+        }
+    }
+}
