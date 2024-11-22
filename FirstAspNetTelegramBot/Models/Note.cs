3c91ffe08a4533c76a47043dba59c9f5dec7bd2c
dagon1093 <dagon1093@gmail.com>
2024-11-22 19:20:15 +0300

    Добавлено поле ChatId к сущности Note

--- FirstAspNetTelegramBot/Models/Note.cs
+++ FirstAspNetTelegramBot/Models/Note.cs
@@ -6,13 +6,16 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Author { get; set; }
+
+        public long ChatId { get; set; }
         public Note() { }
-        public Note(int id, string title, string description, string author)
+        public Note(int id, string title, string description, string author, long chatId)
         {
             Id = id;
             Title = title;
             Description = description;
             Author = author;
+            ChatId = chatId;
         }
 
     }
